464f1be3f36dfb34c3b316f7124053613308add3
Daniel Budi Prasetyo <vyverdrake007@gmail.com>
2025-01-29 16:39:07 +0700

    fix: function documentation and optimization

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import func as f
 import streamlit as st
 
+st.set_page_config(page_title="Deepseek-R1 Chatbot", page_icon="🤖", layout="centered")
 st.title("Deepseek-R1 Chatbot")
+# st.markdown("Welcome to the Deepseek-R1 Chatbot! Feel free to ask me anything. I can help you with a variety of topics, so don't hesitate to start a conversation. Just type your message in the chat box below, and I'll do my best to provide a helpful response.")
+# st.markdown("Let's get started!")
+st.markdown("---")
 
 # Initialize the messages
 if "messages" not in st.session_state:
@@ -22,8 +26,9 @@ if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
     
     # Get the chatbot response
+    response = f.chat_response(prompt)
     with st.chat_message("assistant"):
-       response = st.write_stream(f.chat_response(prompt))
+       st.write_stream(f.response_generator(response))
         
     # Add the chatbot response to the chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
